df81428a7104812987576c2cd1f614fe5271779d
BrunoBMoura <brunomoura.meta@gmail.com>
2023-08-11 09:30:52 -0300

    fixed ram widget.

--- config/widgets/ram.lua
+++ config/widgets/ram.lua
@@ -6,7 +6,7 @@ local spawn = require("awful.spawn")
 
 -- Local widget information.
 local PROC = {
-  cmd = "free | grep Mem",
+  cmd = [[bash -c "free | grep Mem"]],
   match = "(%d+)%s+(%d+)%s+(%d+)",
   interval = 5
 }
@@ -23,22 +23,19 @@ local function worker(user_args)
     font = beautiful.font
   })
 
-  spawn.easy_async_with_shell(PROC.cmd, function(stdout)
-    local total, used, free = stdout:match(PROC.match)
+  spawn.easy_async(PROC.cmd, function(stdout)
+    local total, used, _ = stdout:match(PROC.match)
     local ram_percentage = math.floor((used / total) * 100)
     ram.widget:set_text(string.format("Ram:%s%%", ram_percentage))
   end)
 
   local function update_ram_widget(widget, stdout)
-    local total, used, free = stdout:match(PROC.match)
-    -- local ram_info = get_ram_usage()
+    local total, used, _ = stdout:match(PROC.match)
     local ram_percentage = math.floor((used / total) * 100)
     widget:set_text(string.format("Ram:%s%%", ram_percentage))
   end
 
-  -- update_ram_widget()
-
-  watch(PROC.cmd, 1, update_ram_widget, ram.widget)
+  watch(PROC.cmd, PROC.interval, update_ram_widget, ram.widget)
 
   return ram.widget
 end
