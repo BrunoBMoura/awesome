f4730782be11d986bee138c2813246662cfb842e
BrunoBMoura <brunomoura.meta@gmail.com>
2023-08-16 15:29:45 -0300

    feat: enhancement on powermenu script.

--- scripts/system/rofi_power_menu.sh
+++ scripts/system/rofi_power_menu.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-choice=$(echo -e "Shutdown\nRestart" | rofi -dmenu -i -p "Power Menu")
-
+choice=$(echo -e "Shutdown\nRestart\nLock Screen\nLogout" | rofi -dmenu -i)
 case "$choice" in
-    "Shutdown") sudo shutdown -h now ;;
-    "Restart") sudo shutdown -r now ;;
+    "Shutdown")
+        confirmation=$(echo -e "Yes\nNo" | rofi -dmenu -i -mesg "Shutdown?")
+        case "$confirmation" in
+            "Yes") sudo shutdown -h now ;;
+            *) ;;
+        esac
+        sudo shutdown -h now ;;
+    "Restart")
+        confirmation=$(echo -e "Yes\nNo" | rofi -dmenu -i -mesg "Shutdown?")
+        case "$confirmation" in
+            "Yes") sudo shutdown -r now ;;
+            *) ;;
+        esac
+    "Logout") awesome -q ;;
     *) ;;
 esac
 
